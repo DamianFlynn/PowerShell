15a4d04df65c6f33789c823c73c1c7eae6d20850
Damian Flynn <info@damianflynn.com>
2014-10-14 09:25:42 +0100

    Added New Project for Service Management Automation. Created first Module for SMA management, creating recurring schedules for Runbook automation

---
+++ Scripts/SMA/Create-SMARecurringSchedules.psm1
@@ -0,0 +1,51 @@
+﻿#
+# Create-SMARecurringSchedules.ps1
+#
+
+
+function ADD-SMARecurringSchedule {
+    param (
+        $RecurringInterval = 30,
+        $ScheduleNamePrefix = "Execute Flow at ",
+        $WebServiceEndpoint
+    )
+
+    process {
+        $iterations = 1440 / $interval
+        $endDate   = Get-Date -Month 12 -Day 31 -year 9999 -Hour 5 -Minute 00 -second 00
+        $StartDateUTC = Get-Date -Hour 00 -Minute 00 -Second 00
+        $StartDateEST = Get-Date -Hour 04 -Minute 00 -Second 00
+        $endDate   = Get-Date -Month 12 -Day 31 -year 9999 -Hour 5 -Minute 00 -second 00
+
+        foreach ($iteration in 0..$iterations) {
+            $dateString = get-date $StartDateEST  -Format HH:mm
+            $Name = "$ScheduleNamePrefix $dateString"
+            Write-Output $Name
+            Set-SmaSchedule -StartTime $StartDateUTC -ExpiryTime $endDate -Name $Name -ScheduleType "DailySchedule" -dayInterval 1 -WebServiceEndpoint $WebServiceEndpoint
+    
+            $StartDateEST = $StartDateEST.AddMinutes($interval)
+            $StartDateUTC = $StartDateUTC.AddMinutes($interval)
+        }
+    }
+}
+
+
+function ADD-SMARunbookRecurringSchedule {
+    param (
+        $RecurringInterval = 30,
+        $RunbookName,
+        $WebServiceEndpoint
+    )
+
+    process {
+        $iterations = 1440 / $RecurringInterval
+        $StartDateUTC = Get-Date -Hour 00 -Minute 00 -Second 00
+        foreach ($iteration in 0..$iterations) {
+            $dateString = get-date $StartDateUTC  -Format HH:mm
+            $Name = "Execute Flow at $dateString"
+            Write-Output $Name
+            Start-SMARunbook -Name $RunbookName -webserviceendpoint $WebServiceEndpoint -schedulename $Name
+            $StartDateStr = $StartDateUTC.AddMinutes($RecurringInterval)
+        }
+    }
+}
